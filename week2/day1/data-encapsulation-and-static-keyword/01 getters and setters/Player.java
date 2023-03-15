7494b383d3cd718a93599929ee64ea5c575fd83a
Rikesh Raj <106448477+rikeshraj@users.noreply.github.com>
2023-03-16 02:44:25 +0530

    Create Player.java

---
+++ week2/day1/data-encapsulation-and-static-keyword/01 getters and setters/Player.java
@@ -0,0 +1,22 @@
+public class Player {
+	private String name, country;
+	private int age;
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName(){
+		return this.name;
+	}
+	public void setCountry(String country){
+		this.country = country;
+	}
+	public String getCountry(){
+		return this.country;
+	}
+	public void setAge(int age){
+		this.age = age;
+	}
+	public int getAge(){
+		return this.age;
+	}
+}
