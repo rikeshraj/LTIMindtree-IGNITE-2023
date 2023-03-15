e7fd91dd9d80c638aa6cfc4f3e168188754ba7a2
Rikesh Raj <106448477+rikeshraj@users.noreply.github.com>
2023-03-16 01:43:01 +0530

    Update and rename Q15 Display travel card details based on travel times condition using joins.sql to Q15.sql

--- week2/day5/table-aliases-and-inner-joins/Q15.sql
+++ week2/day5/table-aliases-and-inner-joins/Q15.sql
@@ -1,3 +1,5 @@
+--Display travel card details based on travel times condition using joins
+
 select tc.person_name, tc.contact_number, tc.balance
 from travel_card tc
 inner join travel_payment tp on tc.id = tp.travel_card_id
