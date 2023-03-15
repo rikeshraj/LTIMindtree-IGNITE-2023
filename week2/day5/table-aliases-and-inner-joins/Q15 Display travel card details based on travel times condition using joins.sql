4e3259ca12aa3a9257043aa2f246859bc0c914d1
Rikesh Raj <106448477+rikeshraj@users.noreply.github.com>
2023-03-16 01:38:57 +0530

    Create Q15 Display travel card details based on travel times condition using joins.sql

---
+++ week2/day5/table-aliases-and-inner-joins/Q15 Display travel card details based on travel times condition using joins.sql
@@ -0,0 +1,9 @@
+select tc.person_name, tc.contact_number, tc.balance
+from travel_card tc
+inner join travel_payment tp on tc.id = tp.travel_card_id
+group by tc.person_name, tc.contact_number, tc.balance
+having count(*) = 
+(select max(travel_count)from 
+ (select count(*)as travel_count from travel_payment 
+  group by travel_card_id)as travel_max_count) 
+order by tc.person_name desc;
