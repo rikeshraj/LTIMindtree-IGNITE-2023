69194b87b6d51a8b18ff503bdc9e388a115dafdd
Rikesh Raj <106448477+rikeshraj@users.noreply.github.com>
2023-03-16 02:46:49 +0530

    Create Main.java

---
+++ week2/day1/data-encapsulation-and-static-keyword/02 display item type/Static members - Illustration/Main.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.lang.*;
+public class Main{
+    public static void main(String[]args) throws Exception { 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter Object 1 details\nEnter i1");
+        int i1 = Integer.parseInt(br.readLine());
+        System.out.println("Enter i2");
+        int i2 = Integer.parseInt(br.readLine());
+        StaticIllustration si1 = new StaticIllustration();
+        si1.setI1(i1);
+        si1.setI2(i2);
+        System.out.print("Object 1 ");
+        si1.displayDetails();
+
+        System.out.println("Enter Object 2 details\nEnter i1");
+        i1 = Integer.parseInt(br.readLine());
+        System.out.println("Enter i2");
+        i2 = Integer.parseInt(br.readLine());
+        StaticIllustration si2 = new StaticIllustration();
+        si2.setI1(i1);
+        si2.setI2(i2);
+        System.out.print("Object 2 ");
+        si2.displayDetails();
+        System.out.print("Object 1 ");
+        si1.displayDetails();
+        br.close();
+    }
+}
+    
