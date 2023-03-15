0f48f472892eb1bf46e7f2801ea9f4e82e1a0007
Rikesh Raj <106448477+rikeshraj@users.noreply.github.com>
2023-03-16 03:04:40 +0530

    Create Address.java

---
+++ week2/day1/concept-of-classes-and-objects/01 customer address/Address.java
@@ -0,0 +1,8 @@
+public class Address{
+		public String street, city, country;
+		public int pincode;
+		
+		void displayAddress() {
+			System.out.println("Street: " + street + "\nCity: " + city + "\nPincode: " + pincode + "\nCountry: " + country);
+		}
+}
