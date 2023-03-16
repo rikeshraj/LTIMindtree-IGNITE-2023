2be04835f754763ea2b7c84b990b5d9395d7c18e
Rikesh Raj <106448477+rikeshraj@users.noreply.github.com>
2023-03-16 19:24:49 +0530

    Delete week2/day4/set-operators directory

--- week2/day4/set-operators/Q1.sql
+++
@@ -1,6 +0,0 @@
---Display building details using set operator
-
-select * from building where owner_name='muzzammil' 
-union 
-select * from building where owner_name='nicholas'
-order by owner_name, email_address;
