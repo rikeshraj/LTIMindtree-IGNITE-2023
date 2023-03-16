096137174e2ac74575930f168ce5abcf450517b5
Rikesh Raj <106448477+rikeshraj@users.noreply.github.com>
2023-03-16 21:00:46 +0530

    Create 03.java

---
+++ week1/day2/wriring-first-prog-and-primitives/03.java
@@ -0,0 +1,13 @@
+//Number of events
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of events hosted in Codissia");
+		int no = sc.nextInt();
+		System.out.println("Number of events hosted in Codissia is " + no);
+		sc.close();
+	}
+}
